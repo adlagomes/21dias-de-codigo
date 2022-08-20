0dcc120e367f35ee6705a1e67958c21bc96416d8
Adla Gomes <adlagomes@gmail.com>
2022-08-20 12:42:40 -0300

    initial commit

--- calculadora/src/Calculator.jsx
+++ calculadora/src/Calculator.jsx
@@ -4,48 +4,72 @@ import "./calculator.css";
 import { useState } from "react";
 
 function Calculator() {
-  const [displayValue, setDisplayValue] = useState(0);
-  const [operator, setOperator] = useState();
+  const [displayValue, setDisplayValue] = useState("0");
+  const [oldValue, setOldValue] = useState();
+  const [operator, setOperator] = useState(null);
 
-  const values = [0, 0]
+  function insertDigit(digit) {
 
-  function clearDisplay() {
-    setDisplayValue(0);
-  }
-
-  function insertDigit(n) {
-    if (n === "," && displayValue.includes(",")) {
+    if (digit === "." && displayValue.includes(".")) {
       return;
     }
-    if (displayValue === 0) {
-      setDisplayValue(n)
-    } else {
-      setDisplayValue(displayValue + n)
+    if (displayValue === "0") {
+      setDisplayValue(digit)
+    }else {
+      setDisplayValue(displayValue + digit)
     }
   }
 
-  function setOperation(e) {
-    if (displayValue !== 0) {
-      
-      setOperator(e)
-      console.log(operator)
-    }
+  function clearMemory() {
+    setDisplayValue("0")
+  }
+
+  function setOperation(op) {
+    const signOperator = op;
+    setOperator(signOperator);
+    setOldValue(displayValue);
+    setDisplayValue("0");
+  }
+
+  function calculate() {
+
+    switch (operator) {
+      case "/":
+        setDisplayValue(parseFloat(oldValue) / parseFloat(displayValue))
+        if (isNaN(oldValue) || !isFinite(displayValue)) {
+          clearMemory();
+        }
+        break;
+      case "+":
+        setDisplayValue(parseFloat(oldValue) + parseFloat(displayValue))
+        break;
+      case "X":
+        setDisplayValue(parseFloat(oldValue) * parseFloat(displayValue))
+        break;
+      case "-":
+        setDisplayValue(parseFloat(oldValue) - parseFloat(displayValue))
+        break;
+      default:
+        break;
+      }
+    setOperator(null);
   }
 
   function setBackspace(){
-
+    // Criar uma maneira de apagar digitos individualmente do display como o botao "backspace"
   }
 
-  function setPercent() {
+  function setPercentage() {
+    // Atribuir calculo matematico com as operações envolvendo porcentagem
     setDisplayValue(displayValue / 100)
   }
   
   return (
     <div className="background-color">
       <Display value={displayValue} />
-      <Button label="AC" click={clearDisplay} operation />
+      <Button label="AC" click={clearMemory} operation />
       <Button label="<X" click={setBackspace} operation />
-      <Button label="%" click={setPercent} operation />
+      <Button label="%" click={setPercentage} operation />
       <Button label="/" click={setOperation} operation onChange={setOperation}/>
       <Button label="7" click={insertDigit}/>
       <Button label="8" click={insertDigit} />
@@ -60,8 +84,8 @@ function Calculator() {
       <Button label="3" click={insertDigit}/>
       <Button label="+" click={setOperation} operation />
       <Button label="0" click={insertDigit}/>
-      <Button label="," click={insertDigit}/>
-      <Button label="=" click={setOperation} orange />
+      <Button label="." click={insertDigit}/>
+      <Button label="=" click={calculate} orange />
     </div>
   );
 }
